7ba46bf82e90713d1f1acf940a9dbaabdd14a0c0
Kiran Kumar <kirankumar021097@gmail.com>
2021-06-13 12:40:58 +0530

    Viewdetails in Admin done

--- Admin/Home.php
+++ Admin/Home.php
@@ -1,3 +1,4 @@
+<?php require '../dbConfig/config.php'; ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
@@ -7,11 +8,21 @@
   </head>
   <body>
     <?php require '../Common/AdminHeader.php'; ?>
+
       <div class="container">
         <h3>Users List</h3>
         <ol>
-          <li><a href="#">User 1</a></li>
-          <li><a href="#">User 2</a></li>
+          <?php
+          $query = "SELECT * FROM users";
+          $query_solution = mysqli_query($con, $query);
+          if($query_solution){
+            while ($rows = mysqli_fetch_array($query_solution)) {
+              ?>
+          <li><a href="ViewDetails.php?name=<?php echo $rows['Username'] ?>" type="submit" name="id" value="<?php echo $rows['Username'] ?>"><?php echo $rows['Username'] ?></a></li>
+        <?php
+        }
+      }
+       ?>
         </ol>
       </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
---
+++ Admin/ViewDetails.php
@@ -0,0 +1,73 @@
+<?php require '../dbConfig/config.php'; ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+  </head>
+  <body>
+    <?php require '../Common/AdminHeader.php'; ?>
+
+    <div class="container">
+      <h4>User : <?php echo $_GET['name'] ?></h4>
+      <div class="container-fluid">
+        <div class="row">
+          <form class="" action="" method="GET">
+          <div class="col-auto">
+            <label for="" class="form-label">Start Date</label>
+            <input type="date" name="startdate" value="">
+          </div>
+          <div class="col-auto">
+            <label for="" class="form-label">End Date</label>
+            <input type="date" name="enddate" value="">
+          </div>
+          <div class="col-auto">
+            <input type="button" class="btn btn-sm btn-primary" name="search" value="Search">
+          </div>
+          </form>
+        </div>
+      </div>
+      <table class="table table-responsive table-stripped table-hover table-sm">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Date</th>
+            <th scope="col">Check-In</th>
+            <th scope="col">Check-Out</th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php
+          $uname = $_GET['name'];
+          $query = "SELECT * from attendence where Username = '$uname'";
+          $query_solution = mysqli_query($con, $query);
+          if ($query_solution) {
+            while ($rows = mysqli_fetch_array($query_solution)) {
+              ?>
+          <tr>
+            <td></td>
+            <td><?php echo $rows['Date'] ?></td>
+            <td><?php echo $rows['Check-In'] ?></td>
+            <td><?php echo $rows['Checkout'] ?></td>
+          </tr>
+          <?php
+        }
+      }
+       ?>
+        </tbody>
+      </table>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+  </body>
+</html>
+<?php
+if (isset($_POST['search'])) {
+  $startdate = $_POST['startdate'];
+  $enddate = $_POST['enddate'];
+  $user = $_GET['name'];
+  header("Location: ViewDetails.php?name=$user&sdate=$startdate&edate=$enddate");
+  echo "<script>window.location.href = 'ViewDetails.php?name=$user&sdate=$startdate&edate=$enddate'</script>";
+}
+
+ ?>
