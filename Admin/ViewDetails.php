20c3860af2adf2b221e266dc496e5abecbe98072
Kiran Kumar <kirankumar021097@gmail.com>
2021-06-21 08:44:47 +0530

    Added pagination for tables

--- Admin/ViewDetails.php
+++ Admin/ViewDetails.php
@@ -7,13 +7,16 @@ $uname = $_GET['name'];
     <meta charset="utf-8">
     <title></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.24/css/jquery.dataTables.min.css">
+    <script src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.min.js"></script>
   </head>
   <body>
     <?php require '../Common/AdminHeader.php'; ?>
 
     <div class="container">
       <h4>User : <?php echo $_GET['name'] ?></h4>
-      <div class="container-fluid">
+      <div class="container-fluid mb-3">
         <div class="row">
           <form class="" action="ViewDetails.php" method="GET">
           <div class="col-auto">
@@ -31,7 +34,7 @@ $uname = $_GET['name'];
           </form>
         </div>
       </div>
-      <table class="table table-responsive table-stripped table-hover table-sm">
+      <table class="table table-responsive table-stripped table-hover table-sm" id="myTable">
         <thead>
           <tr>
             <th scope="col">#</th>
@@ -47,11 +50,11 @@ $uname = $_GET['name'];
             $uname = $_GET['name'];
             $startdate = $_GET['startdate'];
             $enddate = $_GET['enddate'];
-            $query = "SELECT * from attendence where Username = '$uname' AND Date BETWEEN '$startdate' AND '$enddate' ";
+            $query = "SELECT * from attendence where Username = '$uname' AND Date BETWEEN '$startdate' AND '$enddate' ORDER BY ID DESC ";
           }
           else {
             $uname = $_GET['name'];
-            $query = "SELECT * from attendence where Username = '$uname'";
+            $query = "SELECT * from attendence where Username = '$uname' ORDER BY ID DESC";
           }
           $query_solution = mysqli_query($con, $query);
           if ($query_solution) {
@@ -74,6 +77,11 @@ $uname = $_GET['name'];
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
   </body>
 </html>
+<script>
+$(document).ready( function () {
+  $('#myTable').DataTable();
+} );
+</script>
 <?php
 if (isset($_POST['search'])) {
   $startdate = $_GET['startdate'];
--- User/Home.php
+++ User/Home.php
@@ -20,7 +20,10 @@ if($query_solution){
     <meta charset="utf-8">
     <title></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
-  </head>
+    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.24/css/jquery.dataTables.min.css">
+    <script src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.min.js"></script>
+    </head>
   <body>
     <?php require '../Common/UserHeader.php'; ?>
     <div class="container-fluid d-flex align-items-end flex-column bd-highlight mb-3">
@@ -46,8 +49,8 @@ if($query_solution){
     </div>
   <?php } ?>
   </form>
-    <div class="container">
-      <table class="table table-responsive table-stripped table-hover table-sm">
+    <div class="container mt-2">
+      <table class="table table-responsive table-stripped table-hover table-sm" id="myTable">
         <thead>
           <tr>
             <th scope="col">#</th>
@@ -59,7 +62,7 @@ if($query_solution){
         </thead>
         <tbody>
           <?php
-          $query = "SELECT * from attendence where Username = '$uname'";
+          $query = "SELECT * from attendence where Username = '$uname' ORDER BY ID DESC";
           $query_solution = mysqli_query($con, $query);
           if ($query_solution) {
             while ($rows = mysqli_fetch_array($query_solution)) {
@@ -82,6 +85,11 @@ if($query_solution){
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
   </body>
 </html>
+<script>
+$(document).ready( function () {
+  $('#myTable').DataTable();
+} );
+</script>
 <?php
   if (isset($_POST['CheckIn'])) {
     $username = $_SESSION['username'];
